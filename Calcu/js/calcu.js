089a2550f58ce52f7bd24ba52754328840717525
Carlos <20253ds088@utez.edu.mx>
2026-05-28 09:37:40 -0600

    update

--- Calcu/js/calcu.js
+++ Calcu/js/calcu.js
@@ -45,21 +45,17 @@ document.getElementById("multiplicar").addEventListener("click", multiplicacion)
 
 
 //Dividir
-
 function dividir(){
     let inputnumero1 = document.getElementById("Numero1");
-    // 1. Usar Number() para convertir el texto en número real
-    let numero1 = Number(inputnumero1.value);
+    let numero1 = inputnumero1.value;
 
     let inputnumero2 = document.getElementById("Numero2");
-    let numero2 = Number(inputnumero2.value);
+    let numero2 = inputnumero2.value;
 
-    let resultado = (numero1) / (numero2);
+    let resultado = (+numero1) / (+numero2);
 
-    // 2. Corregido: Agregar las comillas para seleccionar el ID "resultado"
     let resultado_p = document.getElementById("resultado");
 
-    // 3. Corregido: Mostrar la variable 'resultado' al final de la cadena
     resultado_p.innerText = "El resultado es: " + resultado;
 }
 
--- Calcu/js/tabla_alumnos.js
+++ Calcu/js/tabla_alumnos.js
@@ -1,38 +1,68 @@
 var yo = {
-    nombre: "Carlos Apreza",
+    nombre: "Carlos",
     edad: 18,
     matricula: 20253088,
-    imagen: "imagen/png"
 
+    imagen: "Img/perrito.jpg"
 };
 
-var bd_alumnos= [];
-bd_alumnos.push(yo);
-bd_alumnos.push({
-        nombre: "Carlos",
-        edad: 18,
-        matricula: 20253088,
-        imagen: "imagen/png"
+var bd_alumnos = [];
 
+bd_alumnos.push(yo);
+
+bd_alumnos.push({
+    nombre: "Carlos",
+    edad: 18,
+    matricula: 20253088,
+    imagen: "Img/pumas.jpg"
+});
+
+function add_alumnos() {
+
+    let alumno = bd_alumnos.shift();
+
+    if(alumno == undefined){
+        alert("No hay alumnos");
+        return;
     }
 
+    let fila = document.createElement("tr");
 
+    fila.innerHTML = `
+        <td>${alumno.nombre}</td>
+        <td>${alumno.edad}</td>
+        <td>${alumno.matricula}</td>
+        <td>
+            <img width="50" src="${alumno.imagen}">
+        </td>
+    `;
 
-)
+    let tabla = document.getElementById("tablaAlumnos");
 
-function  add_alumnos(){
-    let alumno=bd_alumnos.pop();
-    let fila=document.createElement("tr");
-    fila.innerHTML=`
-            <td>${alumno.nombre}</td>
-            <td>${alumno.edad}</td>
-            <td>${alumno.matricula}</td>
-            <td><img  width="50" src="../Img/perrito.jpg"></td>
-            
-            
-            `;
-    let tabla =document.getElementById("tablaAlumnos");
     tabla.appendChild(fila);
-
 }
+
 document.getElementById("add").addEventListener("click", add_alumnos);
+
+function agregar_alumnos(){
+
+    let nombre = document.getElementById("Nombre").value;
+    let edad = document.getElementById("edad").value;
+    let matricula = document.getElementById("Matricula").value;
+    let imagen = document.getElementById("Imagen").value;
+
+    let nuevo_alumno = {
+        nombre: nombre,
+        edad: edad,
+        matricula: matricula,
+        imagen: imagen
+    };
+
+    bd_alumnos.push(nuevo_alumno);
+
+    document.getElementById("Agregar_alumno").reset();
+
+    alert("Alumno creado con exito !");
+}
+
+document.getElementById("add_Alumno").addEventListener("click", agregar_alumnos);
